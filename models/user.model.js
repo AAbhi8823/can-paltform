289b8ad19816140b449afb566306c5fe26138ca5
Abhishek <mecoolonly@gmail.com>
2024-02-05 11:14:07 +0530

    updated

--- models/user.model.js
+++ models/user.model.js
@@ -7,8 +7,8 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   full_name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone_number: { type: String, required: true },
+  email: { type: String, required:false },
+  phone_number: { type: String, required:false },
   gender: { type: String, required: true },
   date_of_birth: { type: Date, required: true },
   agreed_To_Terms: { type: Boolean, required: true, default: false },
--- routes/user.profile.routes.js
+++ routes/user.profile.routes.js
@@ -6,6 +6,7 @@ const userprofile_controller=require("../controllers/user.profile.controllers")
 router.post("/add-user-profile",userprofile_controller.add_user_profile)
 //router.get("/get-user-profile",userprofile_controller.get_user_profile)
 router.get("/get-user-profile-list",userprofile_controller.get_user_profile_list)
+router.post("/user-profile-login",userprofile_controller.user_profile_login)
 
 
 
