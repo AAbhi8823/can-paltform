ef8ffbe94d16dcad89dfa9f19f56ca9924578a34
Abhishek Kumar <97abhishekkumar97@gmail.com>
2024-06-11 13:31:10 +0530

    updated

--- routes/medicine.routes.js
+++ routes/medicine.routes.js
@@ -7,7 +7,7 @@ router.post("/add-medicine",medicine_controller.add_medicine)
 router.put("/update-medicine",medicine_controller.update_medicine)
 router.get("/get-medicine-list",medicine_controller.get_medicine_list)
 router.get('/get-medicine-by-date/:medicine_date',medicine_controller.get_medicine_details_month_wise)
-router.get("/get-medicine-bank/:user_id",medicine_controller.get_medicine_bank_details_month_wise)
+router.get("/get-medicine-bank/",medicine_controller.get_medicine_bank_details_month_wise)
 router.delete("/delete-medicine/:medicine_id",medicine_controller.delete_medicine)
 
 router.get("/get-medicine-by-id/:medicine_id",medicine_controller.get_medicine_details_by_id)
