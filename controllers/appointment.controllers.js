26ee3d6d0cc3d323107fcd873033c78dd057501e
Abhishek Kumar <97abhishekkumar97@gmail.com>
2024-06-27 09:51:59 +0530

    updated

--- controllers/appointment.controllers.js
+++ controllers/appointment.controllers.js
@@ -311,14 +311,20 @@ exports.get_appointment_by_date = [
   * will return the appointments for the next 7 days from the current date
  */
 
-exports.get_appointment_for_next_7_days = [
+ exports.get_appointment_for_next_7_days = [
   login_validator,
   async (req, res) => {
     try {
+      // Get current date and set time to start of the day
       const currentDate = new Date();
+      currentDate.setHours(0, 0, 0, 0);
+
+      // Get date for the next 7 days and set time to end of the day
       const nextWeek = new Date();
       nextWeek.setDate(currentDate.getDate() + 7);
+      nextWeek.setHours(23, 59, 59, 999);
 
+      // Fetch appointments within the next 7 days
       const appointments = await appointment_model.find({
         user_id: req.user.user._id,
         appointment_date: {
@@ -341,7 +347,3 @@ exports.get_appointment_for_next_7_days = [
     }
   },
 ];
-
-
-
-
